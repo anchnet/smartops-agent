0b9e183e94302227f112cb0e0667bf01189cee77
dongjia <papandadj@gmail.com>
2021-08-02 11:59:27 +0800

    fix windows

--- cmd/app/scanendpoint.go
+++ cmd/app/scanendpoint.go
@@ -1,4 +1,5 @@
 // +build !windows
+// +build 386
 
 package app
 
--- pkg/collector/system/disk_windows.go
+++ pkg/collector/system/disk_windows.go
@@ -1,10 +1,9 @@
-// +build !windows
+// +build windows
 
 package system
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/anchnet/smartops-agent/pkg/collector/core"
@@ -28,7 +27,6 @@ func (c *DiskCheck) Collect(t time.Time) ([]metric.MetricSample, error) {
 		return nil, err
 	}
 	samples = append(samples, c.collectPartitionMetrics(partitions, t)...)
-
 	check, ok := core.GetCheck("iostats")
 	if ok {
 		ms, err := check.Collect(t)
@@ -42,11 +40,6 @@ func (c *DiskCheck) Collect(t time.Time) ([]metric.MetricSample, error) {
 }
 
 func (c DiskCheck) exclude(disk disk.PartitionStat) bool {
-	hasPre := strings.HasPrefix(disk.Device, "/dev/")
-	if !hasPre {
-		return true
-	}
-
 	switch disk.Fstype {
 	case "devfs",
 		"devtmpfs",
@@ -79,6 +72,7 @@ func (c DiskCheck) collectPartitionMetrics(partitions []disk.PartitionStat, time
 
 		tag["filesystem"] = partition.Fstype
 		tag["mountpoint"] = partition.Mountpoint
+
 		samples = append(samples, metric.NewServerMetricSample(c.formatMetric("total"), float64(usage.Total), metric.UnitByte, time, tag))
 		samples = append(samples, metric.NewServerMetricSample(c.formatMetric("used"), float64(usage.Used), metric.UnitByte, time, tag))
 		samples = append(samples, metric.NewServerMetricSample(c.formatMetric("free"), float64(usage.Free), metric.UnitByte, time, tag))
